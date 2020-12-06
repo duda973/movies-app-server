4189b95a1031d3a5c09bf22b09339a2e1a8808b9
Dragutin Todorovic <duda.todorovic.97@gmail.com>
2020-12-06 18:08:17 +0100

    Props fix

--- src/main/java/com/moviebase/moviebaseapi/MoviesDatabaseApiApplication.java
+++ src/main/java/com/moviebase/moviebaseapi/MoviesDatabaseApiApplication.java
@@ -1,9 +1,12 @@
 package com.moviebase.moviebaseapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.moviebase")
 public class MoviesDatabaseApiApplication {
 
 	public static void main(String[] args) {
